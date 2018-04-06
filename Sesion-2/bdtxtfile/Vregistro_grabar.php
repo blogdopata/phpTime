a9010eb2f7686799a2d0465cc1b211209ac22f40
blogdopata <vcaballero_c@hotmail.com>
2018-04-06 14:44:43 -0500

    task

---
+++ Sesion-2/bdtxtfile/Vregistro_grabar.php
@@ -0,0 +1,17 @@
+<?php
+$a=$_POST['txtapellidos'];
+$b=$_POST['txtnombre'];
+$c=$_POST['txtcumple'];
+#unimos los datos y agregamos 
+#salto de linea al final
+$linea= "$a|$b|$c" . PHP_EOL;
+
+#abrimos con opcion de crear y anexar datos
+$base=fopen("basedatos.txt","a+");
+fwrite($base,$linea,strlen($linea));
+fclose($base);
+#se redirecciona al usuario
+header('location: EReporte.php');
+
+
+?>
