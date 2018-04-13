e2ff4278b2334fa9f848ebcdc65b5f5e88bcac8a
blogdopata <vcaballero_c@hotmail.com>
2018-04-12 22:09:01 -0500

    practice

---
+++ practice/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Practice</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<main class="container" style="margin-top: 100px;">
+		<section class="row text-center" >
+			<div class="col-sm-6 col-sm-offset-2" style="border:1px solid red;">
+					<form action="login.php" method="post">
+
+						<div>
+							<label for="">Usuario :</label> <input type="text" name="txtnombre"> <br>
+							<label for="">Clave:</label> <input type="password" name ="txtpass">
+								<br>
+
+								<input type="submit" value="loguearse">
+
+						</div>
+						
+
+					</form>
+
+			</div>
+			
+		</section>
+		
+
+
+
+	</main>
+</body>
+</html>
